9ad4ccc34ed1a494ec5d5606be4df3bfe9897c96
DavidKosal <david.kosal@revature.net>
2021-06-21 15:03:50 -0500

    Hacker Rank Week 4

---
+++ W4HackerRankFnameLname/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace W4HackerRankFnameLname
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
